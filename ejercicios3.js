ba65cffa386ae3ba174fce979cb7a2e563cdf9ac
Axel Lionel Ruiz <106162256+Axelruiz92@users.noreply.github.com>
2022-08-15 23:31:56 -0300

    Add files via upload

---
+++ ejercicios3.js
@@ -0,0 +1,66 @@
+
+/*
+for (let i=0; i < 10; i++ ) {
+    console.log ("El valor de i es:", i);
+  }
+*/ 
+
+
+/*
+
+for (let i=0; i < 5; i++ ) {
+  let nombre = prompt ("ingrese su nombre");
+  console.log ("bienvenido/a" , nombre)
+}
+ */
+
+
+
+/*
+for (let i=0; i < 3 ; i++ ) {
+  let numero = parseInt(prompt ("dime un numero, te dire el siguiente"));
+  console.log ("elegiste el numero:" , numero , "el siguiente numero es:", numero+1) 
+}
+ */
+
+
+/*
+let numero = parseInt(prompt("dime un numero del 1 al 10, te dire su tabla"))  
+
+for (let i=0; i < 11 ; i++ ) {
+ 
+  console.log(numero, "x" , i , " = " , numero*i  )
+} 
+*/
+
+///////////////////////////////////////////////////////////////
+
+//BREAK
+/*
+for (let i=0; i < 3 ; i++ ) {
+  let num_uno = parseInt(prompt ("Ingrese el primer numero"));
+  let num_dos = parseInt(prompt ("Ingrese otro numero"));
+
+
+if (num_dos == 0 ) {
+  console.log(" no se puede dividir por 0 ");
+  break
+}
+console.log(" el resultado de la division es ", num_uno/num_dos);
+}
+*/
+
+/*
+
+for (let i=0; i < 3 ; i++ ) {
+  let num_uno = parseInt(prompt ("Ingrese el primer numero"));
+  let num_dos = parseInt(prompt ("Ingrese otro numero"));
+
+
+if (num_dos == 0 ) {
+  console.log(" no se puede dividir por 0 ");
+  continue
+}
+console.log(" el resultado de la division es ", num_uno/num_dos);
+}
+*/
